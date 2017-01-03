211f19d9fa95b6151b069a59a36767b24b66937c
PythonNut <PythonNut@users.noreply.github.com>
2017-01-03 21:51:15 +0000

    Initial Commit

---
+++ VM Backup to Snapdragon VSS.ps1
@@ -0,0 +1,133 @@
+# Self-elevating stub, since VSS snapshots can only be created by an administrator
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+  Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit 
+}
+
+# Query the user for the backup password
+$SecurePassword = Read-Host -Prompt "Enter password" -AsSecureString
+
+# Query the user for the backup mode
+$Title = "Backup Mode"
+$Message = "What backup strategy do you want to use?"
+$Automatic = New-Object System.Management.Automation.Host.ChoiceDescription "&Automatic", `
+    "Backup automatically every 20 minutes."
+$Manual = New-Object System.Management.Automation.Host.ChoiceDescription "&Manual", `
+    "Backup manually on user request."
+$Options = [System.Management.Automation.Host.ChoiceDescription[]]($Automatic, $Manual)
+$Mode = $host.ui.PromptForChoice($Title, $Message, $Options, 0)
+
+switch ($Mode) {
+  0 {$Mode = "auto"}
+  1 {$Mode = "manual"}
+}
+
+
+
+# Detect if the system has a battery (so we can avoid killing it)
+$Battery = $false
+If ($Mode -eq "auto") {
+  Try {
+    If ((Get-WmiObject -Class BatteryStatus -Namespace root\wmi -List) -ne $null) {
+      Write-Host "Detected internal battery"
+      $Battery = True
+    }
+  } 
+  Catch { }
+}
+
+# Detect the Virtual Machine path
+If (Test-Path 'E:\VirtualBox VMs') {
+  Write-Host "Detected external VM folder"
+  $VSSTarget = "E:\.atomic"
+  $VSSDriveLetter = "E"
+  $BorgTarget = "/cygdrive/e/.atomic/VirtualBox\\ VMs/"
+}
+Else {
+  Write-Host "Detected internal VM folder"
+  $VSSTarget = "C:\.atomic"
+  $VSSDriveLetter = "C"
+  $BorgTarget = "/cygdrive/c/.atomic/Users/Pytho/VirtualBox\\ VMs/"
+}
+
+While ($true) {
+  $Counter = 0
+  If ($Battery -and !(Get-WmiObject -Class BatteryStatus -Namespace root\wmi).PowerOnLine) {
+    Write-Host 'On battery power. Plug laptop in or press any key to backup:'
+
+    While (!$Host.UI.RawUI.KeyAvailable) {
+      [Threading.Thread]::Sleep(1000)
+      $Counter++
+      If ($Counter % 60 * 1 -eq 0) {
+        If ((Get-WmiObject -Class BatteryStatus -Namespace root\wmi).PowerOnLine) {
+          Break
+        }
+      }
+    }
+
+    # Clear the pending key
+    If ($host.ui.RawUI.KeyAvailable) {
+      $host.ui.RawUI.ReadKey("IncludeKeyDown,NoEcho")
+    }
+  }
+
+  # Cleanup the VSS link target, just in case
+  If (Test-Path "${VSSTarget}") {
+    cmd /c "rmdir /s /q ${VSSTarget}"
+  }
+
+  # Create a new VSS snapshot and detect its ID
+  $ShadowStatus = (wmic shadowcopy call create Volume="${VSSDriveLetter}:\")
+  $ShadowID = [regex]::match($ShadowStatus, 'ShadowID = "([^"]+)').Groups[1].Value
+
+  Write-Output "Sucessfully created shadow copy at ${ShadowID}."
+
+  Try {
+    # Capture the snapshot's Device Object and "mount" it to VSSTarget
+    $DeviceObject = ([regex]::match((wmic shadowcopy get DeviceObject,ID), "(\S+)\s+$ShadowID").Groups[1].Value) + "\"
+    cmd /c "mklink /d ${VSSTarget} ${DeviceObject}"
+
+    # Check if VMs are online
+    $VMsOffline = (& 'C:\Program Files\Oracle\VirtualBox\VBoxManage' list runningvms) -eq $null
+    $StateSuffix = If ($VMsOnline) { "offline" } Else { "online" } 
+
+    # Decrypt the password in memory
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
+    $PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+    Remove-Variable BSTR
+
+    # Start the backup
+    & "C:\Program Files\Borg\bin\bash.exe" -l -c @"
+    export BORG_PASSPHRASE=${PlainPassword};: 
+    echo Starting borg backup...;: 
+    cd $BorgTarget;: 
+    borg create -vspx -C lz4 pythonnut@snapdragon:/mnt/backup/Borgs/VMsBorg::'{now:%Y.%m.%d-%H.%M.%S}-vms-$StateSuffix-{hostname}-{user}' .;: 
+    echo Starting borg prune...;: 
+    borg prune -vs --list pythonnut@snapdragon:/mnt/backup/Borgs/VMsBorg --keep-within 2H -H 8 -d 7 -w 3;: 
+"@
+    Remove-Variable PlainPassword
+  }
+  Catch {
+    Write-Output "An error has occured. Cleaning up..."
+  }
+  Finally {
+    # Remove the VSS target and delete the VSS snapshot
+    cmd /c "rmdir /s /q ${VSSTarget}"
+    vssadmin delete shadows /shadow=$ShadowID /quiet
+    Write-Output "Successfully deleted shadow copy ${ShadowID}."
+  }
+  
+  If ($Mode -eq "manual") {
+    Read-Host 'Press Enter to backup again...' | Out-Null
+  }
+  Else {
+    Write-Host 'Waiting 20 minutes til next backup... Press any key to backup immediately:'
+
+    While (!$Host.UI.RawUI.KeyAvailable -and ($Counter++ -lt 60 * 1)) {
+      [Threading.Thread]::Sleep(1000)
+    }
+    # Clear the pending key
+    If ($host.ui.RawUI.KeyAvailable) {
+      $host.ui.RawUI.ReadKey("IncludeKeyDown,NoEcho")
+    }
+  }
+}
---
+++ VM Restore from Snapdragon.ps1
@@ -0,0 +1,75 @@
+﻿# Query the user for the backup password
+$SecurePassword = Read-Host -Prompt "Enter password" -AsSecureString
+
+$ExtractRepo = "/mnt/backup/Borgs/VMsBorg"
+$ExtractSource = "/mnt/backup/Borgs/mnt"
+$Location = "pythonnut@snapdragon"
+
+# Detect the Virtual Machine path
+If (Test-Path 'E:\VirtualBox VMs') {
+  Write-Host "Detected external VM folder"
+  $ExtractTarget = "/mnt/e/VirtualBox\\ VMs/"
+}
+Else {
+  Write-Host "Detected internal VM folder"
+  $ExtractTarget = "/mnt/c//Users/Pytho/VirtualBox\\ VMs/"
+}
+
+# Decrypt the password in memory
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
+$PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+Remove-Variable BSTR
+
+# Mount the borg repository
+Try {
+  Write-Host "Getting list of Archives..."
+  $MountpointTest=(& "C:\Program Files\Borg\bin\bash.exe" -l -c "ssh $Location 'ls -1 $ExtractSource'")
+  If ($MountpointTest.count -gt 0) {
+    & "C:\Program Files\Borg\bin\bash.exe" -l -c "ssh $Location 'fusermount -u $ExtractSource'"
+  }
+  Write-Host "Verified mountpoint"
+  & "C:\Program Files\Borg\bin\bash.exe" -l -c "ssh $Location 'BORG_PASSPHRASE=${PlainPassword} borg mount -o allow_other $ExtractRepo $ExtractSource'"
+  Write-Host "Mount completed"
+  $Archives=(& "C:\Program Files\Borg\bin\bash.exe" -l -c "ssh $Location 'ls -1 $ExtractSource'")
+  Write-Host "Archive list retrieved"
+  Write-Host ""
+  Write-Host "Select archive to restore:"
+  For ($I = 0; $I -lt $Archives.count; $I++) {
+    Write-Host ("[{0:00}] {1}" -f $I, $Archives[$I])
+  }
+
+  $Archive = $Archives[(Read-Host 'Enter archive number') -as [int]]
+  Write-Host ("Restoring archive {0}..." -f $Archive)
+
+  $Path = ""
+
+  While ($true) {
+    $DirScan = (& "C:\Program Files\Borg\bin\bash.exe" -l -c "ssh $Location 'ls -A1 ${ExtractSource}/${Archive}/${Path}'")
+    If ($DirScan -eq $null) {
+      Throw "Could not detect VM folder in archive"
+    }
+
+    If ($DirScan -contains "Arch Linux Aurora") {
+      Break
+    }
+
+    If ($DirScan.GetType() -ne "".GetType()) {
+      Throw "Could not detect VM folder in archive"
+    }
+    
+    $Path = $Path + ($DirScan -replace " ","\ ") + "/"
+  }
+
+  Write-Host ("Detected VM folder: {0}." -f $Path)
+
+  bash -c "time rsync -vah -e 'ssh -Tx -c aes128-gcm@openssh.com -o Compression=no' --inplace --info=progress2 --stats --delete-before pythonnut@192.168.0.117:'${ExtractSource}/${Archive}/${Path}' ${ExtractTarget}"
+}
+Catch {
+  Write-Output $_.Exception.Message
+  Write-Output "An error has occured. Cleaning up..."
+}
+Finally {
+  & "C:\Program Files\Borg\bin\bash.exe" -l -c "ssh $Location 'fusermount -u $ExtractSource'"
+  Write-Host "Unmounted filesystem"
+  Read-Host 'Press Enter to exit...' | Out-Null
+}
